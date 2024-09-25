2f81b360a064b659fe46166e453b132c2c40f5a1
Frans Daniel <220711826@students.uajy.ac.id>
2024-09-25 21:47:51 +0700

    Update Tugas_Modul 2_B_DAN_11826.py

--- Tugas_Modul 2_B_DAN_11826.py
+++ Tugas_Modul 2_B_DAN_11826.py
@@ -3,7 +3,8 @@ import streamlit as st
 import pickle
 import os
 
-model_path='GBT_heartDisease_model.pkl'
+model_path = 'C:/Users/LENOVO/Documents/Tugas_B_11826'
+model=os.path.join(model_path, 'GBT_heartDisease_model.pkl')
 
 with open(model, 'rb') as f:
     loaded_model = pickle.load(f)
